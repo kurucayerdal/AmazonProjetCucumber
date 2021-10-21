2a78319363dbcb196b32948dff968bccfbfa1c4e
kurucayerdal <kurucayerdal@gmail.com>
2021-10-21 21:59:06 +0200

    amazon cucumber projet

--- src/test/java/runners/ConnexionRunner.java
+++ src/test/java/runners/ConnexionRunner.java
@@ -8,21 +8,22 @@ import org.testng.annotations.AfterClass;
 @CucumberOptions(
         features = {"src/test/java/features/Connexion.feature"},
         glue = {"steps"},
-        plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/ExtentReport/ExtentReportConnexionPage.html", // oluşturalacak raporun yeri ve adı veriliyor.
+        plugin = { // oluşturalacak raporun yeri ve adı veriliyor.
                 "html:target/cucumber-report", //jenkins için eklendi
                 "json:target/cucumber.json",}
+       //"com.cucumber.listener.ExtentCucumberFormatter:target/ExtentReport/ExtentReportConnexionPage.html",
 )
 
 public class ConnexionRunner extends AbstractTestNGCucumberTests {
 
     @AfterClass
     public static void afterClass() {
-        Reporter.loadXMLConfig("src/test/java/xmlFiles/ExtendReportSet.xml");
-        Reporter.setSystemInfo("User Name", "Erdal Kurucay");
-        Reporter.setSystemInfo("Application Name", "Amazon Connexion Page");
-        Reporter.setSystemInfo("Operating System Type", System.getProperty("os.name").toString());
-        Reporter.setSystemInfo("Department", "QA");
-        Reporter.setTestRunnerOutput("Amazon Test Execution Connexion Page Cucumber Report");
+//        Reporter.loadXMLConfig("src/test/java/xmlFiles/ExtendReportSet.xml");
+//        Reporter.setSystemInfo("User Name", "Erdal Kurucay");
+//        Reporter.setSystemInfo("Application Name", "Amazon Connexion Page");
+//        Reporter.setSystemInfo("Operating System Type", System.getProperty("os.name").toString());
+//        Reporter.setSystemInfo("Department", "QA");
+//        Reporter.setTestRunnerOutput("Amazon Test Execution Connexion Page Cucumber Report");
     }
 
 }
